cd417d86696b2386970723c98ecbffd8df737e17
Enda Lee <enda.lee@ittdublin.ie>
2016-11-08 15:36:40 +0000

    Exercise 4 forms revised

--- src/app/convert-m-to-k/convert-m-to-k.component.ts
+++ src/app/convert-m-to-k/convert-m-to-k.component.ts
@@ -7,15 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvertMToKComponent implements OnInit {
 
-  clickMessage: string = "";
-  clickMessage1: string = "";
+  clickMessage: number;
+  milesToKilometers = 1.60934;
+  clickMessage1: number;
+  kilometerToMiles = 0.621371;
 
   
   constructor() { }
 
-   onClickMe() {
-    document.getElementById('mi-to-ki').innerHTML = document.getElementById('miles').innerText + ' miles is ' + (parseInt(document.getElementById('miles').innerText) * 1.60934) + ' kilometeres!!';
-     document.getElementById('ki-to-mi').innerHTML = document.getElementById('kilometers').innerText + ' kilometers is ' + (parseInt(document.getElementById('kilometers').innerText) * 0.621371) + ' miles!!';
+    onClickMe(myMessage: number){
+    this.clickMessage = myMessage * this.milesToKilometers ;
+  }
+
+  onClickMe1(myMessage1: number){
+    this.clickMessage1 = myMessage1 * this.kilometerToMiles  ;
   }
 
   ngOnInit() {
